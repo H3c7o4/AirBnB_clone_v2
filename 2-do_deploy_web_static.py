5685d8e21138d1ba766184881fe14bff159d4767
H3c7o4 <hectorvladitok@gmail.com>
2023-04-08 10:29:49 +0100

    ch

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to the web servers.
-"""
+from fabric.api import put, run, local, env
 from os import path
-from fabric.api import env, put, run
 
-env.hosts = ['100.26.228.181', '35.153.17.79']
+
+env.hosts = ["52.87.254.153", "34.207.212.10"]
 
 
 def do_deploy(archive_path):
-    """ Function that distributes the archive.
+    """Fabric script that distributes
+    an archive to your web server"""
 
-    Args:
-        archive_path (str): the path of the archive to deploy on the servers.
-    """
-
-    try:
-        if not path.exists(archive_path):
-            raise FileNotFoundError
-
-        name = archive_path.split("/")[-1]
-        name_no_ext = name.split(".")[0]
-
-        remote = "/data/web_static/releases"
-        dest = "{}/{}".format(remote, name_no_ext)
-
-        put(archive_path, '/tmp')
-        run('mkdir -p {}/'.format(dest))
-        run('tar -xzf /tmp/{} -C {}'.format(name, dest))
-        run('rm /tmp/{}'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(dest))
-
-    except:
-        print("Error. Version deploy aborted")
+    if not path.exists(archive_path):
         return False
-
-    print("New version deployed!")
-    return True
+    try:
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
+        return True
+    except Exception as e:
+        return False
