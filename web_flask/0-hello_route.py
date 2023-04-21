d2e63175a03d29804febfe11cf7543d32bb89f6b
H3c7o4 <hectorvladitok@gmail.com>
2023-04-21 09:10:26 +0100

    Create the 0-hello_route.py file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 def hello_HBNB():
     """ Function that returns a message on a web browser """
     return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
